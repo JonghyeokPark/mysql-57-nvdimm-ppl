a8af668522d5a0ba22b430cbd7f59eb800fe38ae
JonghyeokPark <akindo19@skku.edu>
2022-11-08 16:15:40 +0900

    create files for IPL-specific operations and modify CMakeLists.txt for compilation

---
+++ storage/innobase/nvdimm/nvdimm0init.cc
@@ -0,0 +1,3 @@
+// Copyright 2022 VLDB Lab. (http://vldb.skku.ac.kr/)
+// Author: Jonghyeok Park
+// E-mail: akindo19@skku.edu
---
+++ storage/innobase/nvdimm/nvdimm0log.cc
@@ -0,0 +1,3 @@
+// E-mail: akindo19@skku.edu
+// Author: Jonghyeok Park
+// E-mail: akindo19@skku.edu
