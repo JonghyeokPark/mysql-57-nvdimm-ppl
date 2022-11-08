f7b60fe9a7cd94e1e1bd115dc783fddca9b99463
JonghyeokPark <akindo19@skku.edu>
2022-11-08 16:09:17 +0900

    add inintial header for nvdimm

---
+++ storage/innobase/include/nvdimm-ipl.h
@@ -0,0 +1,74 @@
+// Copyright 2022 VLDB Lab. (http://vldb.skku.ac.kr/)
+// Author: Jonghyeok Park
+// E-mail: akindo19@skku.edu
+
+#ifndef __NVDIMM_IPL_H_
+#define __NVDIMM_IPL_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+
+#include <string.h>
+#include <stdint.h>
+#include <assert.h>
+#include <unistd.h>
+#include <cassert>
+#include <iostream>
+
+// TDOO(jhpark): make this variable configurable
+#define NVDIMM_MAP_SIZE	(10*1024*1024*1024UL)
+
+#define NVDIMM_MMAP_FILE_NAME         			"nvdimm_mmap_file"
+#define NVDIMM_MMAP_MAX_FILE_NAME_LENGTH    10000
+
+#define NVDIMM_INFO_PRINT(fmt, args...)              \
+  fprintf(stderr, "[NVDIMM_INFO]: %s:%d:%s():" fmt,  \
+  __FILE__, __LINE__, __func__, ##args)                \
+
+#define NVDIMM_ERROR_PRINT(fmt, args...)             \
+  fprintf(stderr, "[NVDIMM_ERROR]: %s:%d:%s():" fmt, \
+  __FILE__, __LINE__, __func__, ##args)                \
+
+
+// (jhpark): persistence barrier
+#define CACHE_LINE_SIZE 64
+#define ASMFLUSH(dst) __asm__ __volatile__ ("clflush %0" : : "m"(*(volatile char *)dst))
+
+static inline void clflush(volatile char* __p) {
+  asm volatile("clflush %0" : "+m" (*__p));
+}
+
+static inline void mfence() {
+  asm volatile("mfence":::"memory");
+  return;
+}
+
+
+static inline void flush_cache(void *ptr, size_t size){
+  unsigned int  i=0;
+  uint64_t addr = (uint64_t)ptr;
+
+  mfence();
+  for (i =0; i < size; i=i+CACHE_LINE_SIZE) {
+    clflush((volatile char*)addr);
+    addr += CACHE_LINE_SIZE;
+  }
+  mfence();
+}
+static inline void memcpy_persist
+                    (void *dest, void *src, size_t size){
+  unsigned int  i=0;
+  uint64_t addr = (uint64_t)dest;
+
+  memcpy(dest, src, size);
+
+  mfence();
+  for (i =0; i < size; i=i+CACHE_LINE_SIZE) {
+    clflush((volatile char*)addr);
+    addr += CACHE_LINE_SIZE;
+  }
+  mfence();
+}
+
+#endif // end-of-header
