e14b495378e4ad2ba7022ff2daa75a6d46796f3e
JonghyeokPark <akindo19@skku.edu>
2022-11-09 14:05:44 +0900

    temp commit

--- storage/innobase/nvdimm/nvdimm0log.cc
+++ storage/innobase/nvdimm/nvdimm0log.cc
@@ -1,3 +1,21 @@
 // E-mail: akindo19@skku.edu
 // Author: Jonghyeok Park
 // E-mail: akindo19@skku.edu
+
+// TODO(jhpark): Implement Log applier
+// 1. REDO Log based log applier (Merge)
+// 2. Convert LSN-based Redo log -> Per-page Redo log
+// 3. Utilize Change buffer "merge" operation (`ibuf_merge_or_delete_for_page()`)
+// 	- when index page is read from a disk to buffer pool, 
+//	this function applies any buffered operations to the page and deletes the entries from the change buffer
+// 	how to handle doropped pages ( operation is NOT read but page is created ) ??
+
+// 	At least, we may know the appropriate position for perfect timing for merge operation
+//  + Get the idea from data structures to managin per-page log
+
+// Log applier is actually identical to recovery process
+// Call recv_recover_page() function on the fly in normal process  ... ? 
+
+// step1. collect/manage pages's modification into NVDIMM region per-page manner
+// step2. apply per-page log when 
+// step3. for split pages do not invoke IPLization process 
